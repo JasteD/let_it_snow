a07a3290088434a52dc0b084b5dd07d0487e9f24
_JPP_ <janphilipp.paatsch@fau.de>
2022-12-04 15:32:09 +0100

    Changes packeages version in setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,3 @@ setup(
     version='0.1',
     packages=find_packages(include=['snowflake', 'let_it_snow.*'])
 )
-
-setup(
-    name='requirements',
-    packages=find_packages(include=['requirements.*'])
-)
