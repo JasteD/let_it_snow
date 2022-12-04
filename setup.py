bc098e81ae7918afd24038cf7e5f0bb3faf64257
_JPP_ <janphilipp.paatsch@fau.de>
2022-12-04 15:28:24 +0100

    Changes packeages name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='snowflage',
-    version='0.1.0',
+    name='snowflake',
     packages=find_packages(include=['snowflake', 'let_it_snow.*'])
 )
