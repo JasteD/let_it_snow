f0b7b475598f56dc4ed6ad3e73c559e390f5ff69
_JPP_ <janphilipp.paatsch@fau.de>
2022-12-05 00:14:30 +0100

    updated setup.py with install_requires

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='snowflake',
     version='0.1',
     packages=find_packages(include=['snowflake', 'let_it_snow.*']),
-    requires=[
+    install_requires=[
         'numpy',
         'turtle'
     ]
