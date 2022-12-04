687cc914891f21160056e1c8a35983c1fad0f999
_JPP_ <janphilipp.paatsch@fau.de>
2022-12-04 15:37:00 +0100

    Added requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='snowflake',
     version='0.1',
-    packages=find_packages(include=['snowflake', 'let_it_snow.*'])
+    packages=find_packages(include=['snowflake', 'let_it_snow.*']),
+    install_requires=[
+        'numpy',
+        'turtle'
+    ]
 )
